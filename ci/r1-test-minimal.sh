1ba94257e733af22a8fb610f519462827b3b07a7
Nathan Genetzky <nathan@genetzky.us>
2021-07-24 12:20:58 -0500

    ci: r1-test-minimal: Add basic tests

---
+++ ci/r1-test-minimal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source ci/gitlab_console_bitbake.sh
+
+# NOTE: this *must* be done after sourcing 'oe-init-build-env'
+# Posix Strict Mode
+set -eu
+
+# Move out of 'build/'
+cd ../
+
+echo "$0"
+echo "$(basename "$0")"
+cd test/r1-test-minimal
+
+bitbake --version
+
+mkdir -p './tmp/deploy/'
+bitbake -e > ./tmp/deploy/bitbake.bbenv.bb
+
+bitbake-layers show-layers
+bitbake-layers show-recipes
+
+bitbake world
