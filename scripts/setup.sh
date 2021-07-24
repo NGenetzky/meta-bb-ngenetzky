4ec06360d27d0e6a265deecde64806f7fbdf5711
Nathan Genetzky <nathan@genetzky.us>
2021-07-24 13:18:37 -0500

    scripts/setup: Use standardized names, and also symlink project root

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,27 +7,45 @@
 # This is also useful for ensuring that your bootstrapping actually works well.
 
 setup_bitbake(){
-    local bbdir='bitbake'
-    [ -d "${bbdir}/bin/" ] && return
-    git submodule update --init bitbake
+    # local _BITBAKEDIR
+    _BITBAKEDIR='bitbake'
+    [ -d "${_BITBAKEDIR}/bin/" ] && return
+    git submodule update --init "${_BITBAKEDIR}"
 }
 
 setup_builddir(){
-    local bdir='build'
-    [ -d "${bdir}" ] && git clean -Xdff "${bdir}"
+    # local _BUILDDIR _BBLAYERS_FETCH_DIR
+    _BUILDDIR="build"
+    _BBLAYERS_FETCH_DIR="build/layers"
+    [ -d "${_BUILDDIR}" ] && git clean -Xdff "${_BUILDDIR}"
     mkdir -p \
-        "${bdir}/conf/"
+        "${_BUILDDIR}/conf/" \
+        "${_BBLAYERS_FETCH_DIR}"
     cp -T \
         'conf/bblayers.conf' \
-        "${bdir}/conf/bblayers.conf"
-    mkdir -p \
-        "${bdir}/layers/"
-    ln -fsr -t "${bdir}/layers/" \
+        "${_BUILDDIR}/conf/bblayers.conf"
+
+    ln -fsr -t "${_BBLAYERS_FETCH_DIR}" \
+        "./../${PROJECT_NAME}"
+    # primary layers
+    ln -fsr -t "${_BBLAYERS_FETCH_DIR}" \
         meta-*
 }
 
 main(){
-    cd "${PROJDIR?}"
+    if [ -n "${PROJECT_ROOT-}" ]; then
+        PROJECT_ROOT="$(readlink -f "${PROJECT_ROOT}")"
+    else
+        PROJECT_ROOT="$(pwd)"
+    fi
+
+    if [ -z "${PROJECT_NAME-}" ]; then
+        PROJECT_NAME="$(basename ${PROJECT_ROOT})"
+    fi
+
+    # cd to PROJECT_ROOT for consistent context
+    cd "${PROJECT_ROOT?}"
+
     setup_bitbake
     setup_builddir
 }
@@ -38,8 +56,8 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 
     SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 
-    PROJDIR="$(readlink -f "${SCRIPTDIR}"/../)"
-    readonly PROJDIR
+    # cd to PROJECT_ROOT for consistent context
+    cd "$SCRIPTDIR/../"
 
     set -x
     main "$@"
