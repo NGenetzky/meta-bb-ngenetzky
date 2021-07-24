cb1d81906ef07b248451c3cafbddf3ff2d28279d
Nathan Genetzky <nathan@genetzky.us>
2021-07-24 13:57:00 -0500

    scripts/setup: Implement TEMPLATECONF for bblayers.conf

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,11 +6,57 @@
 #
 # This is also useful for ensuring that your bootstrapping actually works well.
 
+# "NOTE: OECOMPAT" marks sections designed for compatibility with OE.
+# In an attempt to be more compatible with other projects that use
+# bitbake (openembeded, yocto, poky) we support their variable names.
+
 setup_bitbake(){
     # local _BITBAKEDIR
     _BITBAKEDIR='bitbake'
     [ -d "${_BITBAKEDIR}/bin/" ] && return
     git submodule update --init "${_BITBAKEDIR}"
+}
+
+setup_build_conf(){
+    _BUILDDIR="build"
+
+    # NOTE: OECOMPAT
+    #
+    # $TEMPLATECONF can point to a directory for the template local.conf & bblayers.conf
+    #
+    if [ -n "${TEMPLATECONF-}" ]; then
+        if [ ! -d "$TEMPLATECONF" ]; then
+            # Allow TEMPLATECONF=meta-xyz/conf as a shortcut
+            if [ -d "$OEROOT/$TEMPLATECONF" ]; then
+                TEMPLATECONF="$OEROOT/$TEMPLATECONF"
+            fi
+            if [ ! -d "$TEMPLATECONF" ]; then
+                echo >&2 "Error: TEMPLATECONF value points to nonexistent directory '$TEMPLATECONF'"
+                exit 1
+            fi
+        fi
+    else
+        TEMPLATECONF="${PROJECT_ROOT}"
+    fi
+
+    # NOTE: OECOMPAT
+    if [ -f "$TEMPLATECONF/bblayers.conf.sample" ]; then
+        # Put the abosolute path to the layers in bblayers.conf so we can run
+        # bitbake without the init script after the first run
+        # ##COREBASE## is deprecated as it's meaning was inconsistent, but continue
+        # to replace it for compatibility.
+        sed -e "s|##OEROOT##|$OEROOT|g" \
+            -e "s|##COREBASE##|$OEROOT|g" \
+            "$TEMPLATECONF/bblayers.conf.sample" \
+            > "${_BUILDDIR}/conf/bblayers.conf"
+    fi
+
+    # We would prefer to use relative symlinks for our layers.
+    ln -fsr -t "${_BBLAYERS_FETCH_DIR}" \
+        "./../${PROJECT_NAME}"
+    # primary layers
+    ln -fsr -t "${_BBLAYERS_FETCH_DIR}" \
+        meta-*
 }
 
 setup_builddir(){
@@ -21,15 +67,8 @@ setup_builddir(){
     mkdir -p \
         "${_BUILDDIR}/conf/" \
         "${_BBLAYERS_FETCH_DIR}"
-    cp -T \
-        'conf/bblayers.conf' \
-        "${_BUILDDIR}/conf/bblayers.conf"
 
-    ln -fsr -t "${_BBLAYERS_FETCH_DIR}" \
-        "./../${PROJECT_NAME}"
-    # primary layers
-    ln -fsr -t "${_BBLAYERS_FETCH_DIR}" \
-        meta-*
+    setup_build_conf
 }
 
 main(){
@@ -39,12 +78,15 @@ main(){
         PROJECT_ROOT="$(pwd)"
     fi
 
+    # cd to PROJECT_ROOT for consistent context
+    cd "${PROJECT_ROOT?}"
+
     if [ -z "${PROJECT_NAME-}" ]; then
         PROJECT_NAME="$(basename ${PROJECT_ROOT})"
     fi
 
-    # cd to PROJECT_ROOT for consistent context
-    cd "${PROJECT_ROOT?}"
+    # NOTE: OECOMPAT
+    OEROOT="${PROJECT_ROOT}"
 
     setup_bitbake
     setup_builddir
